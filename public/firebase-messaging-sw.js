7c31127347a39f5b8fc373d1876df5044b882442
user01 <uuser01@MacBookPro-2.home>
2026-02-09 00:47:00 +0100

    fix: 정적 검사(Lint) 전역 식별자 선언 및 코드 클리닝 🧼🧹

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,4 @@
+/* global firebase, importScripts */
 // public/firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
